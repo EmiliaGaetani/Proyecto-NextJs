b320e12aeae9f971c62b8ac788c084733a08c8ee
EmiliaGaetani <egaetani@pioix.edu.ar>
2025-08-19 08:50:17 -0300

    Cambios

--- proyecto-emiju/src/components/Button.js
+++ proyecto-emiju/src/components/Button.js
@@ -1,9 +1,9 @@
 "use client"
 import clsx from "clsx"
+import styles from "@/components/Button.module.css"
 
 export default function Button(props) {
   return (
-
     <>
       <button className={
       clsx(
